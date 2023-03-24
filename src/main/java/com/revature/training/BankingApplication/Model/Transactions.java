f0095cd76b62ee207c0c66bb4631f3078788a701
RECamp88 <recamp88@gmail.com>
2023-03-24 10:29:46 -0500

    set Transaction Model variables back to private

--- src/main/java/com/revature/training/BankingApplication/Model/Transactions.java
+++ src/main/java/com/revature/training/BankingApplication/Model/Transactions.java
@@ -14,13 +14,13 @@ import java.sql.Timestamp;
 public class Transactions {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public long transactionId;
-    public int posted_to;
-    public double deposit_amount;
-    public double withdrawal_amount;
+    private long transactionId;
+    private int posted_to;
+    private double deposit_amount;
+    private double withdrawal_amount;
 
     @CreationTimestamp
-    public Timestamp time_stamp;
+    private Timestamp time_stamp;
 
 
 
